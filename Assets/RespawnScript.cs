31cea5f92812caefe4ca17bdb999eb156dee8360
matorsoni <matorsoni45@gmail.com>
2019-02-07 00:16:52 +0100

    Adds respawn

---
+++ Assets/RespawnScript.cs
@@ -0,0 +1,16 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnScript : MonoBehaviour {
+
+    [SerializeField]
+    private Transform playerPos;
+    [SerializeField]
+    private Transform respawnPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerPos.transform.position = respawnPos.transform.position;
+    }
+}
